117775024d62723ec84ca615f1c494f398b3edd3
Buton Damien <buton.damien@gmail.com>
2022-07-18 17:48:15 +0200

    Manage unit testing

--- AppTest.php
+++ AppTest.php
@@ -2,129 +2,22 @@
 
 namespace App;
 
-require_once("./Entity/Card.php");
-require_once("./Service/GameManager.php");
+use App\Test\HandAnalyzerTest;
 
-use App\Entity\Card;
-use App\Service\GameManager;
+require_once("./Test/HandAnalyzerTest.php");
 
-$gameManager = new GameManager();
+$handAnalyzerTest = new HandAnalyzerTest();
 
-//High Card Test
-$hand = [
-    new Card('2', 'C'),
-    new Card('8', 'D'),
-    new Card('6', 'S'),
-    new Card('A', 'C'),
-    new Card('K', 'C'),
-];
-
-$result = $gameManager->analyzeHand($hand, 'High Card Test');
-$gameManager->displayCards($hand);
-$gameManager->displayHandResult($result);
+if (isset($argv[1])) {
+    if (!in_array($argv[1], HandAnalyzerTest::$handTypeTest)) {
+        echo "Parameter must be : " . implode(',', HandAnalyzerTest::$handTypeTest) . "\n";
+    } else {
+        $function = $argv[1].'Test';
+        $handAnalyzerTest->$function();
+    }
+} else {
+    $handAnalyzerTest->testAll();
+}
 
 
-//Pair Test
-$hand = [
-    new Card('2', 'C'),
-    new Card('A', 'D'),
-    new Card('6', 'S'),
-    new Card('A', 'C'),
-    new Card('K', 'C'),
-];
 
-$result = $gameManager->analyzeHand($hand, 'Pair Test');
-$gameManager->displayCards($hand);
-$gameManager->displayHandResult($result);
-
-//Two Pair Test
-$hand = [
-    new Card('2', 'C'),
-    new Card('A', 'D'),
-    new Card('K', 'S'),
-    new Card('A', 'C'),
-    new Card('K', 'C'),
-];
-
-$result = $gameManager->analyzeHand($hand, 'Two Pair Test');
-$gameManager->displayCards($hand);
-$gameManager->displayHandResult($result);
-
-//Three of kind Test
-$hand = [
-    new Card('2', 'C'),
-    new Card('8', 'D'),
-    new Card('K', 'S'),
-    new Card('K', 'D'),
-    new Card('K', 'C'),
-];
-
-$result = $gameManager->analyzeHand($hand, 'Three of kind Test');
-$gameManager->displayCards($hand);
-$gameManager->displayHandResult($result);
-
-
-//Straight Test
-$hand = [
-    new Card('6', 'C'),
-    new Card('7', 'D'),
-    new Card('8', 'S'),
-    new Card('9', 'D'),
-    new Card('T', 'C'),
-];
-
-$result = $gameManager->analyzeHand($hand, 'Straight Test');
-$gameManager->displayCards($hand);
-$gameManager->displayHandResult($result);
-
-
-//Flush Test
-$hand = [
-    new Card('2', 'D'),
-    new Card('7', 'D'),
-    new Card('A', 'D'),
-    new Card('9', 'D'),
-    new Card('K', 'D'),
-];
-
-$result = $gameManager->analyzeHand($hand, 'Straight Test');
-$gameManager->displayCards($hand);
-$gameManager->displayHandResult($result);
-
-//Full House Test
-$hand = [
-    new Card('3', 'C'),
-    new Card('3', 'D'),
-    new Card('3', 'S'),
-    new Card('T', 'D'),
-    new Card('T', 'C'),
-];
-
-$result = $gameManager->analyzeHand($hand, 'Full house Test');
-$gameManager->displayCards($hand);
-$gameManager->displayHandResult($result);
-
-//Straight Flush Test
-$hand = [
-    new Card('3', 'C'),
-    new Card('3', 'D'),
-    new Card('3', 'S'),
-    new Card('3', 'D'),
-    new Card('T', 'C'),
-];
-
-$result = $gameManager->analyzeHand($hand, 'Four of kind Test');
-$gameManager->displayCards($hand);
-$gameManager->displayHandResult($result);
-
-$hand = [
-    new Card('3', 'C'),
-    new Card('4', 'C'),
-    new Card('5', 'C'),
-    new Card('6', 'C'),
-    new Card('7', 'C'),
-];
-
-$result = $gameManager->analyzeHand($hand, 'Straight Flush Test');
-$gameManager->displayCards($hand);
-$gameManager->displayHandResult($result);
---
+++ Test/HandAnalyzerTest.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace App\Test;
+
+require_once("./Service/GameManager.php");
+
+use App\Entity\Card;
+use App\Service\GameManager;
+
+class HandAnalyzerTest
+{
+    private GameManager $gameManager;
+
+    public static array $handTypeTest = [
+        'highCard',
+        'pair',
+        'twoPair',
+        'threeOfKind',
+        'straight',
+        'flush',
+        'fullHouse',
+        'straightFlush',
+    ];
+
+    public function __construct()
+    {
+        $this->gameManager = new GameManager();
+    }
+
+    public function testAll() {
+        $this->highCardTest();
+        $this->pairTest();
+        $this->twoPairTest();
+        $this->threeOfKindTest();
+        $this->straightTest();
+        $this->flushTest();
+        $this->fourOfKindTest();
+        $this->straightFlushTest();
+    }
+
+    public function highCardTest() {
+
+        $hand = [
+            new Card('2', 'C'),
+            new Card('8', 'D'),
+            new Card('6', 'S'),
+            new Card('A', 'C'),
+            new Card('K', 'C'),
+        ];
+
+        $result = $this->gameManager->analyzeHand($hand, 'High Card Test');
+        $this->gameManager->displayCards($hand);
+        $this->gameManager->displayHandResult($result);
+    }
+
+    public function pairTest()
+    {
+        $hand = [
+            new Card('2', 'C'),
+            new Card('A', 'D'),
+            new Card('6', 'S'),
+            new Card('A', 'C'),
+            new Card('K', 'C'),
+        ];
+
+        $result = $this->gameManager->analyzeHand($hand, 'Pair Test');
+        $this->gameManager->displayCards($hand);
+        $this->gameManager->displayHandResult($result);
+    }
+
+    public function twoPairTest()
+    {
+        $hand = [
+            new Card('2', 'C'),
+            new Card('A', 'D'),
+            new Card('K', 'S'),
+            new Card('A', 'C'),
+            new Card('K', 'C'),
+        ];
+
+        $result = $this->gameManager->analyzeHand($hand, 'Two Pair Test');
+        $this->gameManager->displayCards($hand);
+        $this->gameManager->displayHandResult($result);
+    }
+
+    public function threeOfKindTest()
+    {
+        $hand = [
+            new Card('2', 'C'),
+            new Card('8', 'D'),
+            new Card('K', 'S'),
+            new Card('K', 'D'),
+            new Card('K', 'C'),
+        ];
+
+        $result = $this->gameManager->analyzeHand($hand, 'Three of kind Test');
+        $this->gameManager->displayCards($hand);
+        $this->gameManager->displayHandResult($result);
+    }
+
+    public function straightTest()
+    {
+        $hand = [
+            new Card('6', 'C'),
+            new Card('7', 'D'),
+            new Card('8', 'S'),
+            new Card('9', 'D'),
+            new Card('T', 'C'),
+        ];
+
+        $result = $this->gameManager->analyzeHand($hand, 'Straight Test');
+        $this->gameManager->displayCards($hand);
+        $this->gameManager->displayHandResult($result);
+    }
+
+    public function flushTest()
+    {
+        $hand = [
+            new Card('2', 'D'),
+            new Card('7', 'D'),
+            new Card('A', 'D'),
+            new Card('9', 'D'),
+            new Card('K', 'D'),
+        ];
+
+        $result = $this->gameManager->analyzeHand($hand, 'Flush Test');
+        $this->gameManager->displayCards($hand);
+        $this->gameManager->displayHandResult($result);
+    }
+
+    public function fullHouseTest()
+    {
+        $hand = [
+            new Card('3', 'C'),
+            new Card('3', 'D'),
+            new Card('3', 'S'),
+            new Card('T', 'D'),
+            new Card('T', 'C'),
+        ];
+
+        $result = $this->gameManager->analyzeHand($hand, 'Full house Test');
+        $this->gameManager->displayCards($hand);
+        $this->gameManager->displayHandResult($result);
+    }
+
+    public function fourOfKindTest()
+    {
+        $hand = [
+            new Card('3', 'C'),
+            new Card('3', 'D'),
+            new Card('3', 'S'),
+            new Card('3', 'D'),
+            new Card('T', 'C'),
+        ];
+
+        $result = $this->gameManager->analyzeHand($hand, 'Four of kind Test');
+        $this->gameManager->displayCards($hand);
+        $this->gameManager->displayHandResult($result);
+    }
+
+    public function straightFlushTest()
+    {
+        $hand = [
+            new Card('3', 'C'),
+            new Card('4', 'C'),
+            new Card('5', 'C'),
+            new Card('6', 'C'),
+            new Card('7', 'C'),
+        ];
+
+        $result = $this->gameManager->analyzeHand($hand, 'Straight Flush Test');
+        $this->gameManager->displayCards($hand);
+        $this->gameManager->displayHandResult($result);
+    }
+}
